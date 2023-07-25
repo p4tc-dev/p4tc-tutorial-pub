62a3a3c1bc117225b654595e245f4d7be4e71dc4
Tomasz Osinski <tomasz.osinski@intel.com>
2023-07-25 17:44:50 +0200

    Increase memory and fix libs

--- vm/root-release-bootstrap.sh
+++ vm/root-release-bootstrap.sh
@@ -9,12 +9,13 @@ apt-get update -q
 
 apt-get install -qq -y --no-install-recommends --fix-missing \
   ca-certificates curl git net-tools python3 python3-pip tcpdump unzip \
-  vim wget make gcc libc6-dev flex bison libelf-dev libssl-dev
+  vim wget make gcc libc6-dev flex bison libelf-dev libssl-dev dpkg-dev build-essential debhelper \
+  pkg-config
 
 # Compile and install kernel with P4TC support
 git clone https://github.com/p4tc-dev/linux-p4tc-pub.git
 cd linux-p4tc-pub/
 cp /home/vagrant/config-p4tc-x86-ubuntu-22.04 .config
 make olddefconfig
-make -j `nproc`
+make -j`nproc`
 make modules_install && make install
